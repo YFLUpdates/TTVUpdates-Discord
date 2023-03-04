0724f57b66a69f7f0299987dea85ddac01279acf
xanax <34755589+anKordii@users.noreply.github.com>
2023-03-04 22:52:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -103,7 +103,20 @@ async function getMembers() {
 }
 
 client.on("messageCreate", async (msg) => {
-  if (msg.channelId === channel && Array.from(msg.attachments).length > 0) {
+  if(msg.channelId === "1075498439435104316" && msg.author.id !== "1071108766843556020"){
+    if(Array.from(msg.attachments).length === 0){
+      msg.delete();
+
+      msg.channel.send(`» To nie jest zdjęcie <@${msg.author.id}>`).then(msg => setTimeout(() => msg.delete(), 5000));
+
+      return;
+    }
+
+    msg.channel.send(`» <@${msg.author.id}>, twój zrzut ekranu oczekuje na weryfikację moderacji. <@&1006911102002671625> <@&1006910777728454686>`);
+
+    return;
+
+  }else if (msg.channelId === channel && Array.from(msg.attachments).length > 0) {
     await saveStrefa(msg.author.id, msg.author.username);
 
     const embed = new EmbedBuilder()
