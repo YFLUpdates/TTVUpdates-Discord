cf25fd8ab9db78a95c72928f0b088568aa8fd1f1
ziomiwang <ziomiwang@gmail.com>
2023-03-20 18:15:47 +0100

    slots initial commit

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import { Client, GatewayIntentBits, EmbedBuilder } from "discord.js";
-import express from "express";
+import { Client, EmbedBuilder, GatewayIntentBits } from "discord.js";
 import dotenv from "dotenv";
+import express from "express";
 import { promises as fs } from "fs";
+import middleware from "./authz/docchi-server.js";
+import pointsCom from "./command/points.js";
+import multiplyDice from "./functions/multiplyDice.js";
+import rollDice from "./functions/rollDice.js";
+import { slotsInfo } from "./functions/slots/data/slotsData.js";
+import { runSlots } from "./functions/slots/slots.js";
+import gambleUpdate from "./requests/gambleUpdate.js";
 
 import getPoints from "./requests/getPoints.js";
-import rollDice from "./functions/rollDice.js";
-import multiplyDice from "./functions/multiplyDice.js";
-import middleware from "./authz/docchi-server.js";
-import gambleUpdate from "./requests/gambleUpdate.js";
-import pointsCom from "./command/points.js";
 
 dotenv.config();
 
@@ -110,16 +112,18 @@ async function getMembers() {
 }
 
 client.on("messageCreate", async (msg) => {
-  const args = msg.content.slice(1).split(' ');
-	const command = args.shift().toLowerCase();
-  const argumentClean = args[0] ? (args[0].replaceAll("@", "").toLowerCase()):(null)
+  const args = msg.content.slice(1).split(" ");
+  const command = args.shift().toLowerCase();
+  const argumentClean = args[0]
+    ? args[0].replaceAll("@", "").toLowerCase()
+    : null;
 
-  if(["dice", "kosci"].includes(command)){
-    if(msg.channelId !== gambleChannel){
+  if (["dice", "kosci"].includes(command)) {
+    if (msg.channelId !== gambleChannel) {
       return;
     }
 
-    if (cooldown > (Date.now() - 2000)) {
+    if (cooldown > Date.now() - 2000) {
       return;
     }
     cooldown = Date.now();
@@ -127,20 +131,32 @@ client.on("messageCreate", async (msg) => {
     const discordID = msg.author.id;
     const points = await getPoints(discordID, "adrian1g__");
 
-    if(!argumentClean){
-        return msg.channel.send(`<@${discordID}>, zapomniałeś/aś o kwocie `); 
+    if (!argumentClean) {
+      return msg.channel.send(`<@${discordID}>, zapomniałeś/aś o kwocie `);
     }
 
-    if(Number(argumentClean) > 5000 || Number(argumentClean) <= 0 || isNaN(argumentClean)){
-        return msg.channel.send(`<@${discordID}>, maksymalnie można obstawić 5000 punktów `); 
+    if (
+      Number(argumentClean) > 5000 ||
+      Number(argumentClean) <= 0 ||
+      isNaN(argumentClean)
+    ) {
+      return msg.channel.send(
+        `<@${discordID}>, maksymalnie można obstawić 5000 punktów `
+      );
     }
 
-    if(points === null || points.points === null){
-      return msg.channel.send("<@"+ discordID +"> najprawdopodobniej nie połączyłeś bota ze swoim kontem `!connectdc "+discordID+"` na kanale adrian1g__");
+    if (points === null || points.points === null) {
+      return msg.channel.send(
+        "<@" +
+        discordID +
+        "> najprawdopodobniej nie połączyłeś bota ze swoim kontem `!connectdc " +
+        discordID +
+        "` na kanale adrian1g__"
+      );
     }
 
-    if(Number(argumentClean) > points.points){
-        return msg.channel.send(`<@${discordID}> nie masz tylu punktów aha `);
+    if (Number(argumentClean) > points.points) {
+      return msg.channel.send(`<@${discordID}> nie masz tylu punktów aha `);
     }
 
     const dice1 = await rollDice();
@@ -149,37 +165,129 @@ client.on("messageCreate", async (msg) => {
     const betPoints = Number(argumentClean);
     const multiplyAmount = multiplyDice(dice1, dice2, dice3);
 
-    if(multiplyAmount === null){
-        const updatePoints = await gambleUpdate("adrian1g__", `-${betPoints}`, points.user_login)
+    if (multiplyAmount === null) {
+      const updatePoints = await gambleUpdate(
+        "adrian1g__",
+        `-${betPoints}`,
+        points.user_login
+      );
 
-        if(updatePoints === null){
-            return msg.channel.send( `<@${discordID}> coś się rozjebało przy aktualizowaniu punktów <:aha:1014651386505465896> `);
-        }
+      if (updatePoints === null) {
+        return msg.channel.send(
+          `<@${discordID}> coś się rozjebało przy aktualizowaniu punktów <:aha:1014651386505465896> `
+        );
+      }
 
-        return msg.channel.send(`<@${discordID}> przegrałeś/aś wszystko <:jasperSmiech:1026122842976309402> - 🎲${dice1} 🎲${dice2} 🎲${dice3}`);
+      return msg.channel.send(
+        `<@${discordID}> przegrałeś/aś wszystko <:jasperSmiech:1026122842976309402> - 🎲${dice1} 🎲${dice2} 🎲${dice3}`
+      );
     }
 
-    const winAmount = (betPoints * multiplyAmount);
-    const updatePoints = await gambleUpdate("adrian1g__", `+${winAmount - betPoints}`, points.user_login)
+    const winAmount = betPoints * multiplyAmount;
+    const updatePoints = await gambleUpdate(
+      "adrian1g__",
+      `+${winAmount - betPoints}`,
+      points.user_login
+    );
 
-    if(updatePoints === null){
-        return msg.channel.send(`<@${discordID}> coś się rozjebało przy aktualizowaniu punktów <:aha:1014651386505465896> `);
+    if (updatePoints === null) {
+      return msg.channel.send(
+        `<@${discordID}> coś się rozjebało przy aktualizowaniu punktów <:aha:1014651386505465896> `
+      );
     }
 
-    if(multiplyAmount === 66){
-        return msg.channel.send( `<@${discordID}> szatańska wygrana ${winAmount} <:okurwa:1016741160779268166> FIRE x66 - 🎲${dice1} 🎲${dice2} 🎲${dice3} `);
+    if (multiplyAmount === 66) {
+      return msg.channel.send(
+        `<@${discordID}> szatańska wygrana ${winAmount} <:okurwa:1016741160779268166> FIRE x66 - 🎲${dice1} 🎲${dice2} 🎲${dice3} `
+      );
     }
 
-    if(multiplyAmount === 33){
-        return msg.channel.send(`<@${discordID}> szczęśliwa trójka ${winAmount} PartyKirby 🍀 🍀 x33 - 🎲${dice1} 🎲${dice2} 🎲${dice3} `);
+    if (multiplyAmount === 33) {
+      return msg.channel.send(
+        `<@${discordID}> szczęśliwa trójka ${winAmount} PartyKirby 🍀 🍀 x33 - 🎲${dice1} 🎲${dice2} 🎲${dice3} `
+      );
     }
 
-    return msg.channel.send(`<@${discordID}> wygrałeś/aś ${winAmount} punktów <:okurwa:1016741160779268166> - 🎲${dice1} 🎲${dice2} 🎲${dice3}`);
-  }else if(["yflpoints", "punkty", "points"].includes(command)){
-    const commands = await pointsCom("adrian1g__", msg.author.id, argumentClean, args);
+    return msg.channel.send(
+      `<@${discordID}> wygrałeś/aś ${winAmount} punktów <:okurwa:1016741160779268166> - 🎲${dice1} 🎲${dice2} 🎲${dice3}`
+    );
+  } else if (["slot", "slots", "slotsy"].includes(command)) {
+    const discordID = msg.author.id;
 
+    if (msg.channelId !== gambleChannel) {
+      return;
+    }
+
+    if (cooldown > Date.now() - 2000) {
+      return;
+    }
+    cooldown = Date.now();
+
+    if (argumentClean === "info") {
+      return msg.channel.send(`<@${discordID}>, ${slotsInfo}`);
+    }
+
+    const points = await getPoints(discordID, "adrian1g__");
+
+    if (!argumentClean) {
+      return msg.channel.send(`<@${discordID}>, zapomniałeś/aś o kwocie `);
+    }
+
+    const betPoints = Number(argumentClean);
+    if (betPoints > 20000 || betPoints <= 0 || !betPoints) {
+      return msg.channel.send(
+        `<@${discordID}>, maksymalnie można obstawić 20000 punktów `
+      );
+    }
+    if (points === null || points.points === null) {
+      return msg.channel.send(
+        "<@" +
+        discordID +
+        "> najprawdopodobniej nie połączyłeś bota ze swoim kontem `!connectdc " +
+        discordID +
+        "` na kanale adrian1g__"
+      );
+    }
+    if (betPoints > points.points) {
+      return msg.channel.send(`<@${discordID}> nie masz tylu punktów aha `);
+    }
+    const outcome = runSlots(betPoints);
+    const finalPointsResult = outcome.totalWin - betPoints;
+    let updatePoints;
+
+    if (finalPointsResult <= 0) {
+      updatePoints = await gambleUpdate(
+        "adrian1g__",
+        `-${betPoints}`,
+        points.user_login
+      );
+    } else {
+      updatePoints = await gambleUpdate(
+        "adrian1g__",
+        `+${finalPointsResult}`,
+        points.user_login
+      );
+    }
+
+    if (updatePoints === null) {
+      return msg.channel.send(
+        `<@${discordID}> coś się rozjebało przy aktualizowaniu punktów <:aha:1014651386505465896> `
+      );
+    }
+
+    return msg.channel.send(`<@${discordID}>, ${outcome.message}`);
+  } else if (["yflpoints", "punkty", "points"].includes(command)) {
+    const commands = await pointsCom(
+      "adrian1g__",
+      msg.author.id,
+      argumentClean,
+      args
+    );
     msg.channel.send(commands);
-  }else if (msg.channelId === channel && Array.from(msg.attachments).length > 0) {
+  } else if (
+    msg.channelId === channel &&
+    Array.from(msg.attachments).length > 0
+  ) {
     await saveStrefa(msg.author.id, msg.author.username);
 
     const embed = new EmbedBuilder()
@@ -194,7 +302,10 @@ client.on("messageCreate", async (msg) => {
       .get(channel)
       .send({ embeds: [embed] })
       .catch(console.error);
-  } else if (msg.channelId === "1069641216637014139" && ["czlonkowie"].includes(command)) {
+  } else if (
+    msg.channelId === "1069641216637014139" &&
+    ["czlonkowie"].includes(command)
+  ) {
     const embed = new EmbedBuilder()
       .setTitle("Członkowie")
       .setImage(
@@ -214,15 +325,15 @@ client.on("messageCreate", async (msg) => {
   } else if (["zglos"].includes(command)) {
     const senderID = msg.author.id;
 
-    if(!args[0] || Number(args[0]) <= 0 || isNaN(args[0])){
+    if (!args[0] || Number(args[0]) <= 0 || isNaN(args[0])) {
       return msg.channel.send(`<@${senderID}>, zapomniałeś/aś o kordach X `);
     }
 
-    if(!args[1] || Number(args[1]) <= 0 || isNaN(args[1])){
+    if (!args[1] || Number(args[1]) <= 0 || isNaN(args[1])) {
       return msg.channel.send(`<@${senderID}>, zapomniałeś/aś o kordach Y `);
     }
 
-    if(!args[2] || Number(args[2]) <= 0 || isNaN(args[2])){
+    if (!args[2] || Number(args[2]) <= 0 || isNaN(args[2])) {
       return msg.channel.send(`<@${senderID}>, zapomniałeś/aś o kordach Z `);
     }
 
@@ -232,12 +343,12 @@ client.on("messageCreate", async (msg) => {
         "https://cdn.discordapp.com/attachments/721911008213598238/1083879111388303360/Newsy_2.0.png"
       )
       .setColor(8086271)
-      .setThumbnail('https://i.imgur.com/qzzZEVX.png')
+      .setThumbnail("https://i.imgur.com/qzzZEVX.png")
       .addFields(
-        { name: 'Kordy cuboida', value: `${args[0]}, ${args[1]}, ${args[2]}` },
-        { name: 'Komenda', value: `/tp ${args[0]} ${args[1]} ${args[2]}` },
-        { name: 'Zgłaszający', value: `<@${senderID}>`},
-      )
+        { name: "Kordy cuboida", value: `${args[0]}, ${args[1]}, ${args[2]}` },
+        { name: "Komenda", value: `/tp ${args[0]} ${args[1]} ${args[2]}` },
+        { name: "Zgłaszający", value: `<@${senderID}>` }
+      );
 
     client.channels.cache
       .get("1083775118209187910")
