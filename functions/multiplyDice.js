b947d4cc1f25dc1a35e1d258140b49ff5bf11b72
xanax <34755589+anKordii@users.noreply.github.com>
2023-03-20 18:54:55 +0100

    Update multiplyDice.js

--- functions/multiplyDice.js
+++ functions/multiplyDice.js
@@ -9,13 +9,16 @@ const multiplyDice = function(dice1, dice2, dice3) {
     return 33;
   }
 
+  //25%
   if(sum === 4 || sum === 8 || sum === 12 || sum === 16){
     return 2;
   }
 
-  if(sum === 5 || sum === 10 || sum === 15){
-    return 2;
-  }
+  //43%
+
+  // if(sum === 5 || sum === 10 || sum === 15){
+  //   return 2;
+  // }
 
   // if((dice1 + dice2 + dice3) === 7 || (dice1 + dice2 + dice3) === 14){
   //   return 2;
