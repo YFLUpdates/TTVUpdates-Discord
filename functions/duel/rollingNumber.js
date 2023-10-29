5f8c968e0aa621667db09bd6e60d906b741ca156
sniperpl <87908771+sniperpl@users.noreply.github.com>
2023-10-29 02:53:36 +0200

    upsR

---
+++ functions/duel/rollingNumber.js
@@ -0,0 +1,18 @@
+import sha256 from "sha256";
+import * as math from "mathjs";
+import randomStr from "../randomStr.js";
+
+export default async function rollingNumber() {
+  const hash = randomStr(
+    64,
+    "123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM"
+  );
+  const currentRolllotery = randomStr(7, "123456789");
+  let roll = 0;
+  let sh = sha256(hash + "-" + currentRolllotery);
+  roll = sh.substr(0, 8);
+  roll = parseInt(roll, 16);
+  roll = math.abs(roll) % 2;
+
+  return roll + 1;
+}
